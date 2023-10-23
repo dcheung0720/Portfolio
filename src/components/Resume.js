94c77343c8e1846cd3b2a9f30ada1309f811e184
David <davidcheung231@gmail.com>
2023-10-23 01:51:29 -0500

    added close modal button

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,7 +1,10 @@
+import { Button } from "react-bootstrap";
+
 const Resume = ({handleModalView}) =>{
     return <div style = {{position: "absolute", top: 0, left: 0, right: 0,bottom: 0, 
     width: "100vw", height: "99vh", zIndex: 100, backgroundColor: "rgba(255,255,255,0.3)"}}
     onClick={handleModalView}>
+         <Button style = {{position: "absolute", top: 10, right: 30}}variant="danger">X</Button>
          <embed style = {{width: "70vw", marginTop: "10px"}} scale="tofit" src="/Portfolio/files/resume.pdf" type="application/pdf" width="100%" height="100%"></embed>
     </div>
 };
